3c3dc51805c9f3b7ce2cecb10037ea88afb0c56e
shigetaaa <acrosstanabata@gmail.com>
2024-06-09 13:31:24 +0900

    AWS:Mixed Contentsエラー解消

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,20 +3,24 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\TrustProxies; // 追加
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
-            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class, TrustProxies::class, // 追加
+
         ]);
 
-        //
+        //AWSでCSSやJSが読み込めないエラー解消：AWS向け：公式記載あり：すべてのプロキシを信頼する設定
+        $middleware->trustProxies(at: '*'); // 追加
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

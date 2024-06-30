309de40b885562a582cebbe1df169b4d07500395
shigetaaa <acrosstanabata@gmail.com>
2024-06-30 17:16:50 +0900

    個別表示のリンク不具合修正

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -62,7 +62,7 @@ const Welcome = ({ groupReports }: { groupReports: GroupReports }) => {
                         // console.log(report);  // ここを追加デバッグ用後で消す
                         return (
                             <div key={report.id}>
-                                <Link href={`/${report.username}/reports/${groupReports[groupName].group_slug}/${report.random_id}`}>
+                                <Link href={`/reports/${report.username}/${groupReports[groupName].group_slug}/${report.random_id}`}>
                                     <h3>{report.date}</h3>
                                     <p>{report.title}</p>
                                 </Link>
@@ -81,46 +81,8 @@ const Welcome = ({ groupReports }: { groupReports: GroupReports }) => {
                 </div>
             ))}
         </div>
-
-        //     {Object.keys(groupReports).map((groupName) => (
-        //         <div key={groupName}>
-        //             <h2>{groupName}</h2>
-        //             {groupReports[groupName].map((report) => (
-        //                 <div key={report.id}>
-        //                     <Link href={`/reports/${report.id}`}>
-        //                         <h3>{report.date}</h3>
-        //                         <p>{report.title}</p>
-        //                     </Link>
-        //                 </div>
-        //             ))}
-        //         </div>
-        //     ))}
-        // </div>
     );
 };
 
 
 export default Welcome;
-
-
-
-// export default function Welcome({ auth, reports }: PageProps & GlobalReportsProps) {
-//     return (
-//         <AuthenticatedLayoutHome
-//             user={auth.user}
-//             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
-//         >
-//             <Head title="ほうれんそう" />
-
-//             <div className="py-12">
-//                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-//                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-//                         <div className="p-6 text-gray-900">You're logged in!</div>
-//                         {/* <GlobalReports reports={reports} /> */}
-//                     </div>
-//                 </div>
-//             </div>
-//         </AuthenticatedLayoutHome>
-//         // <h1>ほうれんそう</h1>
-//     );
-// }
